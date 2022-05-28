89c9e7c0928e3f84acd907b217b5677b397bace1
Hassanul50 <hasanakib27@gmail.com>
2022-05-28 22:28:38 +0600

    first attemp

---
+++ TeachingManagementSystem/Models/StudentViewModel/StudentCreateVM.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TeachingManagementSystem.Models.StudentViewModel
+{
+    public class StudentCreateVM
+    {
+        public int StudentId { get; set; }
+
+        [Required]
+        public string? StudentFirstName { get; set; }
+        [Required(ErrorMessage = "Please give your last Name")]
+        public string? StudentLastName { get; set; }
+        public string? StudentEmail { get; set; }
+        public string? Gender { get; set; }
+        public int MobileNumber { get; set; }
+
+
+
+
+
+
+    }
+}
